524e397a1b3ab08d5a43228cbb7a7da367dc5231
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-11-15 15:21:21 -0800

    Version1TicTacToe

--- javaScript.js
+++ javaScript.js
@@ -5,94 +5,276 @@ const scoreDisplay = document.getElementById("scoreDisplay")
 let curPScore = 0
 let curBScore = 0
 let jsBoard = []
+let botSquares = []
 let start = true
+let pause = false
+let gameStopper = false
+let round = 0
 
 function gridSquare(index, tileValue){
     this.index = index
     this.tileValue = tileValue
 }
-//SET UP AS OBJECTS SET UP AS OBJECTS
+//CHANGE ARRAY TO 3 ARRAYS REPRESENTING ROWS
 function createBoard(){
     board.innerHTML = ""
     jsBoard = []
+    jsRows = []
     start = true
+    pause = false
+    gameStopper = false
+    round = 0
     for(let i = 0; i < 9; i++){
+        jsRows.push(new gridSquare(`square_${i}`, 0))
+        if(i === 2 || i === 5 || i === 8){
+            jsBoard.push(jsRows)
+            jsRows = []
+        }
         const domSquare = document.createElement("div")
         domSquare.addEventListener("click", playRound)
         domSquare.classList.add("boardSquare")
         domSquare.setAttribute("id", `square_${i}`)
-        jsBoard.push(new gridSquare(`square_${i}`, undefined))
         board.appendChild(domSquare)
     }
 }
 
 function playRound(){
-    console.log(this.classList.item(1))
-    for(tile in jsBoard){
-        if(jsBoard[tile].index === this.id && jsBoard[tile].tileValue === undefined){
-            jsBoard[tile].tileValue = 0
-            const addText = document.createElement("p")
-            addText.innerText = "X"
-            this.appendChild(addText)
+    let pMove
+    for(row in jsBoard){
+        for(tile in jsBoard[row]){
+            if(jsBoard[row][tile].index === this.id && jsBoard[row][tile].tileValue === 0 && pause != true){
+                pMove = [row, tile]
+                jsBoard[row][tile].tileValue = 1
+                const addText = document.createElement("p")
+                addText.innerText = "X"
+                this.appendChild(addText)
+                round += 1
+            }
         }
     }
-    checkPlayerMoves()
-    botRound()
+    if(pMove != undefined){
+        checkPlayerMoves()
+        botRound(pMove)
+    }
+    if(pause === false && round === 5){
+        pause = true
+        gameStopper = true
+        setTimeout(createBoard, 1000)
+    }
 }
-//Check win conditions after all moves in a round
+
 function checkPlayerMoves(){
     //Checking player horizontals
-    if(
-    (jsBoard[0].tileValue === 0 && jsBoard[1].tileValue === 0 && jsBoard[2].tileValue === 0)||
-    (jsBoard[3].tileValue === 0 && jsBoard[4].tileValue === 0 && jsBoard[5].tileValue === 0)||
-    (jsBoard[6].tileValue === 0 && jsBoard[7].tileValue === 0 && jsBoard[8].tileValue === 0)
-    ){
-        curPScore += 1
-        setTimeout(createBoard, 1000)
+    if(gameStopper === false){
+        if(
+            (jsBoard[0].reduce((a, b)=> a+b.tileValue, 0) === 3)||
+            (jsBoard[1].reduce((a, b)=> a+b.tileValue, 0) === 3)||
+            (jsBoard[2].reduce((a, b)=> a+b.tileValue, 0) === 3)
+        ){
+            curPScore += 1
+            setTimeout(createBoard, 1000)
+            gameStopper = true
+            pause = true
+            
+        }
     }
     //Checking player verticals
-    if(
-        (jsBoard[0].tileValue === 0 && jsBoard[3].tileValue === 0 && jsBoard[6].tileValue === 0)||
-        (jsBoard[1].tileValue === 0 && jsBoard[4].tileValue === 0 && jsBoard[7].tileValue === 0)||
-        (jsBoard[2].tileValue === 0 && jsBoard[5].tileValue === 0 && jsBoard[8].tileValue === 0)
-    ){
-        curPScore += 1
-        setTimeout(createBoard, 1000)
+    if(gameStopper === false){
+        for(column in jsBoard[0]){
+            if(jsBoard[0][column].tileValue+jsBoard[1][column].tileValue+jsBoard[2][column].tileValue === 3){
+                curPScore += 1
+                setTimeout(createBoard, 1000)
+                gameStopper = true
+                pause = true
+            }
+        }
     }
     //Checking player diagonals
-    if(
-        (jsBoard[0].tileValue === 0 && jsBoard[4].tileValue === 0 && jsBoard[8].tileValue === 0)||
-        (jsBoard[2].tileValue === 0 && jsBoard[4].tileValue === 0 && jsBoard[6].tileValue === 0)
-    ){
-        curPScore += 1
-        setTimeout(createBoard, 1000)
+    if(gameStopper === false){
+        if(
+            (jsBoard[0][0].tileValue+jsBoard[1][1].tileValue+jsBoard[2][2].tileValue === 3)||
+            (jsBoard[2][0].tileValue+jsBoard[1][1].tileValue+jsBoard[0][2].tileValue === 3)
+        ){
+            curPScore += 1
+            setTimeout(createBoard, 1000)
+            pause = true
+            gameStopper = true
+        }
     }
     scoreDisplay.innerText = `You: ${curPScore} Bot: ${curBScore}`
 }
-function botRound(){
-    const addText = document.createElement("p")
+
+
+
+function botRound(playerMove){
+    let botDirection = Math.floor(Math.random() * 3)
+    let botText = document.createElement("p")
+    botText.innerText = "O"
     let botChoice
-    addText.innerText = "O"
-    if(start === true){
-        let firstMove = Math.floor(Math.random() * 9)
-        if(jsBoard[firstMove].tileValue === 0){
-            if(firstMove === 0){
-                firstMove = 4
+    //botDirection ===    0 = horizontal, 1 = vertical, 2 = diagonal
+    //3Steps, checks if it can win and if it can't tries to block the player on that directional, if it cant, picks first non occupied square
+    if(pause === false){
+        if(botDirection === 0){
+            for(i in jsBoard){
+                if(jsBoard[i].reduce((a, b)=> a+b.tileValue, 0) === -2){
+                    for(item in jsBoard[i]){
+                        if(item.tileValue === 0){
+                            botChoice = item.index
+                        }
+                    }
+                }
             }
-            else{
-                firstMove = 0
+            if(botChoice === undefined){
+                for(i in jsBoard){
+                    if(botChoice === undefined){
+                        if(jsBoard[i].reduce((a, b)=> a+b.tileValue, 0) >= 1){
+                            for(item in i){
+                                if(item.tileValue === 0){
+                                    botChoice = item.index
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if(botChoice === undefined){
+                for(i in jsBoard[playerMove[0]]){
+                    if(i.tileValue === 0){
+                        botChoice = i.index
+                    }
+                }
             }
         }
-        botChoice = document.getElementById(`square_${firstMove}`)
-        jsBoard[firstMove].tileValue = 1
-        start = false
+        else if(botDirection === 1){
+            for(column in jsBoard[0]){
+                if(jsBoard[0][column]+jsBoard[1][column]+jsBoard[2][column] === -2){
+                    if(jsBoard[0][column].tileValue === 0){
+                        botChoice = jsBoard[0][column].index
+                    }
+                    else if(jsBoard[1][column].tileValue === 0){
+                        botChoice = jsBoard[1][column].index
+                    }
+                    else if(jsBoard[2][column].tileValue === 0){
+                        botChoice = jsBoard[2][column].index
+                    }
+                }
+            }
+            if(botChoice === undefined){
+                for(column in jsBoard[0]){
+                    if(jsBoard[0][column]+jsBoard[1][column]+jsBoard[2][column] >= 1){
+                        if(jsBoard[0][column].tileValue === 0){
+                            botChoice = jsBoard[0][column].index
+                        }
+                        else if(jsBoard[1][column].tileValue === 0){
+                            botChoice = jsBoard[1][column].index
+                        }
+                        else if(jsBoard[2][column].tileValue === 0){
+                            botChoice = jsBoard[2][column].index
+                        }
+                    }
+                }
+            }
+            if(botChoice === undefined){
+                for(i in jsBoard){
+                    if(jsBoard[i][playerMove[1]].tileValue === 0){
+                        botChoice = jsBoard[i][playerMove[1]].index
+                    }
+                }
+            }
+        }
+        else if(botDirection === 2){
+            if(
+                (jsBoard[0][0].tileValue+jsBoard[1][1].tileValue+jsBoard[2][2].tileValue === -2)||
+                (jsBoard[2][0].tileValue+jsBoard[1][1].tileValue+jsBoard[0][2].tileValue === -2)||
+                (jsBoard[0][0].tileValue+jsBoard[1][1].tileValue+jsBoard[2][2].tileValue >= 1)||
+                (jsBoard[2][0].tileValue+jsBoard[1][1].tileValue+jsBoard[0][2].tileValue >= 1)
+            ){
+                switch(0){
+                    case jsBoard[0][0].tileValue:
+                        botChoice = jsBoard[0][0].index
+                        break;
+                    case jsBoard[1][1].tileValue:
+                        botChoice = jsBoard[1][1].index
+                        break;
+                    case jsBoard[2][2].tileValue:
+                        botChoice = jsBoard[2][2].index
+                        break;
+                    case jsBoard[2][0].tileValue:
+                        botChoice = jsBoard[2][0].index
+                        break;
+                    case jsBoard[0][2].tileValue:
+                        botChoice = jsBoard[0][2].index
+                        break;
+                    default:{
+                        for(i in jsBoard){
+                            for(item in jsBoard[i]){
+                                if(jsBoard[i][item].tileValue === 0){
+                                    botChoice = jsBoard[i][item].index
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(botChoice === undefined){
+            for(i in jsBoard){
+                for(item in jsBoard[i]){
+                    if(jsBoard[i][item].tileValue === 0){
+                        botChoice = jsBoard[i][item].index
+                    }
+                }
+            }
+        }
+        for(i in jsBoard){
+            for(item in jsBoard[i]){
+                if(jsBoard[i][item].index === botChoice){
+                    jsBoard[i][item].tileValue = -1
+                }
+            }
+        }
+        if(botChoice != null){
+            botChoice = document.getElementById(botChoice)
+            botChoice.appendChild(botText)
+        }
     }
-    else{
-
+    //Checking bot horizontals
+    if(gameStopper === false){
+        if(
+        (jsBoard[0].reduce((a, b)=> a+b.tileValue, 0) === -3)||
+        (jsBoard[1].reduce((a, b)=> a+b.tileValue, 0) === -3)||
+        (jsBoard[2].reduce((a, b)=> a+b.tileValue, 0) === -3)
+        ){
+        curBScore += 1
+        setTimeout(createBoard, 1000)
+        gameStopper = true
+        pause = true
+        }
     }
-    botChoice.appendChild(addText)
+    //Checking bot verticals
+    if(gameStopper === false){
+        for(column in jsBoard[0]){
+            if(jsBoard[0][column].tileValue+jsBoard[1][column].tileValue+jsBoard[2][column].tileValue === -3){
+                curBScore += 1
+                setTimeout(createBoard, 1000)
+                gameStopper = true
+                pause = true
+            }
+        }
+    }
+    //Checking bot diagonals
+    if(gameStopper === false){
+        if(
+            (jsBoard[0][0].tileValue+jsBoard[1][1].tileValue+jsBoard[2][2].tileValue === -3)||
+            (jsBoard[2][0].tileValue+jsBoard[1][1].tileValue+jsBoard[0][2].tileValue === -3)
+        ){
+            curBScore += 1
+            setTimeout(createBoard, 1000)
+            pause = true
+            gameStopper = true
+        }
+    }
+    scoreDisplay.innerText = `You: ${curPScore} Bot: ${curBScore}`
 }
-
-
 
 createBoard()
